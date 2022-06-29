62e5c5dc0acb2b2e464a4b7e95d5939cbb2faf99
malgo-slota <malgo.slota@gmail.com>
2022-06-29 16:10:09 +0200

    add checkbox

--- app/src/components/Item.js
+++ app/src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item ({ item }) {
+    const[toggle, setToggle] = useState(item.complete);
+
+    const isChecked = () => {
+        setToggle(prev => !prev);
+    }
+
     return (
         <li className="list-group-item">
-            {item.name}
+             <input type="checkbox" 
+                    name="chck"
+                    onClick={isChecked}
+                    className={`form-check-input me-1 ${toggle ? 'active' : ""}`}>
+             </input>
+            <label htmlFor="chck">
+                {item.name}
+            </label>           
         </li>
     );
 }
 
-export default Item;
+export default Item; 
